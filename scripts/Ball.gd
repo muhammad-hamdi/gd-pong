c55e0cc31e5e53a796b3379954619b2f7ae942ee
Muhammad Hamdi <prime.energy7@gmail.com>
2023-02-26 23:10:05 +0200

    added particle to ball and glow, limited speed increase to a max speed

--- scripts/Ball.gd
+++ scripts/Ball.gd
@@ -1,6 +1,7 @@
 extends KinematicBody2D
 
 export var speed = 300
+export var max_speed = 500
 var velocity = Vector2()
 
 const DEFAULT_POSITION = Vector2(512, 300)
@@ -19,7 +20,9 @@ func reset() -> void:
 func _physics_process(delta: float) -> void:
 	var collision = move_and_collide(velocity * speed * delta)
 	if collision:
-		speed += speed *0.05
+		if speed <= max_speed:
+			speed += speed *0.05
+			print(speed)
 		var motion = collision.remainder.bounce(collision.normal)
 		velocity = velocity.bounce(collision.normal)
 		move_and_collide(motion)
