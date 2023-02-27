aa5cb9a9216833ea5467d6dbd9e45e6c5c7e4e78
Muhammad Hamdi <prime.energy7@gmail.com>
2023-02-27 13:17:00 +0200

    working on the power up and the mechanics

--- scripts/PaddleControl.gd
+++ scripts/PaddleControl.gd
@@ -1,17 +1,19 @@
 extends KinematicBody2D
 
-const DEFAULT_SPEED = 400
+export var DEFAULT_SPEED = 400
 export var speed = 400
 
 var direction : float = 0
 var toss: bool = false
 var velocity := Vector2()
+var initial_x: float
+var powered: bool = false
 
 export(String, "right", "left") var side = "right"
 export(String, "player", "ai") var control = "player"
 
 func _ready() -> void:
-	pass
+	initial_x = position.x
 
 func _input(event: InputEvent) -> void:
 	if control == "player":
@@ -24,6 +26,7 @@ func _input(event: InputEvent) -> void:
 				toss = Input.is_action_just_pressed("left_toss")
 
 func _physics_process(delta: float) -> void:
+	position.x = initial_x
 	velocity.y = direction * speed
 	if toss:
 		$AnimationPlayer.play("shake")
@@ -34,6 +37,17 @@ func _physics_process(delta: float) -> void:
 		var body = collision.collider as PhysicsBody2D
 		if body != null && body.is_in_group("ball"):
 			if $AnimationPlayer.current_animation == "shake":
-				speed = DEFAULT_SPEED * 1.5
-				yield(get_tree().create_timer(5.0), "timeout")
+				if !powered:
+					speed = DEFAULT_SPEED * 1.5
+					$SpritePowered.show()
+					powered = true
+			else:
 				speed = DEFAULT_SPEED
+				$SpritePowered.hide()
+				powered = false
+#				$Sprite.material.set_shader_param("interpolation", 0.5)
+#				$Sprite.material.set_shader_param("interpolation", 0)
+
+
+func _on_BallDetector_body_entered(body: Node) -> void:
+	print(body.name)
